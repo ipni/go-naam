6f4e26a1e9503d1797c90586d607aa3cbda66119
gammazero <gammazero@users.noreply.github.com>
2024-03-01 00:05:58 -0800

    Simplify options
    - Simpler options functions
    - Documentation

--- options.go
+++ options.go
@@ -13,7 +13,6 @@ import (
 	"github.com/ipld/go-ipld-prime/datamodel"
 	"github.com/ipld/go-ipld-prime/linking"
 	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
-	"github.com/ipni/go-libipni/dagsync"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/host"
 )
@@ -21,44 +20,40 @@ import (
 const defaultIndexerURL = "https://cid.contact"
 
 type (
-	Option  func(*options) error
+	Option  func(*options)
 	options struct {
-		h              host.Host
-		httpListenAddr string
-		httpIndexerURL string
-		httpFindURL    string
-		httpClient     *http.Client
-		ls             *ipld.LinkSystem
 		ds             datastore.Datastore
-		pub            dagsync.Publisher
+		host           host.Host
+		httpClient     *http.Client
+		httpFindURL    string
+		httpIndexerURL string
+		httpListenAddr string
+		linkSys        *ipld.LinkSystem
 		readPriv       bool
 	}
 
-	PublishOption  func(*publishOptions) error
+	PublishOption  func(*publishOptions)
 	publishOptions struct {
+		embedPubKey bool
 		eol         time.Time
 		ttl         time.Duration
-		embedPubKey bool
 	}
 )
 
 func newOptions(o ...Option) (*options, error) {
 	opts := options{
-		h:              nil,
+		httpClient:     http.DefaultClient,
 		httpIndexerURL: defaultIndexerURL,
 		httpListenAddr: "0.0.0.0:8080",
-		httpClient:     http.DefaultClient,
 		readPriv:       true,
 	}
 	for _, apply := range o {
-		if err := apply(&opts); err != nil {
-			return nil, err
-		}
+		apply(&opts)
 	}
 
 	var err error
-	if opts.h == nil {
-		opts.h, err = libp2p.New()
+	if opts.host == nil {
+		opts.host, err = libp2p.New()
 		if err != nil {
 			return nil, err
 		}
@@ -66,23 +61,8 @@ func newOptions(o ...Option) (*options, error) {
 	if opts.ds == nil {
 		opts.ds = sync.MutexWrap(datastore.NewMapDatastore())
 	}
-	if opts.ls == nil {
-		ls := cidlink.DefaultLinkSystem()
-		ds := namespace.Wrap(opts.ds, datastore.NewKey("ls"))
-		ls.StorageReadOpener = func(ctx linking.LinkContext, l datamodel.Link) (io.Reader, error) {
-			val, err := ds.Get(ctx.Ctx, datastore.NewKey(l.Binary()))
-			if err != nil {
-				return nil, err
-			}
-			return bytes.NewBuffer(val), nil
-		}
-		ls.StorageWriteOpener = func(ctx ipld.LinkContext) (io.Writer, ipld.BlockWriteCommitter, error) {
-			buf := bytes.NewBuffer(nil)
-			return buf, func(l ipld.Link) error {
-				return ds.Put(ctx.Ctx, datastore.NewKey(l.Binary()), buf.Bytes())
-			}, nil
-		}
-		opts.ls = &ls
+	if opts.linkSys == nil {
+		opts.linkSys = makeLinkSys(opts.ds)
 	}
 
 	if opts.httpFindURL == "" {
@@ -92,35 +72,45 @@ func newOptions(o ...Option) (*options, error) {
 	return &opts, nil
 }
 
+func makeLinkSys(ds datastore.Datastore) *ipld.LinkSystem {
+	linkSys := cidlink.DefaultLinkSystem()
+	lsds := namespace.Wrap(ds, datastore.NewKey("ls"))
+	linkSys.StorageReadOpener = func(ctx linking.LinkContext, l datamodel.Link) (io.Reader, error) {
+		val, err := lsds.Get(ctx.Ctx, datastore.NewKey(l.Binary()))
+		if err != nil {
+			return nil, err
+		}
+		return bytes.NewBuffer(val), nil
+	}
+	linkSys.StorageWriteOpener = func(ctx ipld.LinkContext) (io.Writer, ipld.BlockWriteCommitter, error) {
+		buf := bytes.NewBuffer(nil)
+		return buf, func(l ipld.Link) error {
+			return lsds.Put(ctx.Ctx, datastore.NewKey(l.Binary()), buf.Bytes())
+		}, nil
+	}
+	return &linkSys
+}
+
+// WithDatastore supplies an existing datastore. An in-memory datastore is
+// created if one is not supplied.
+func WithDatastore(ds datastore.Datastore) Option {
+	return func(o *options) {
+		o.ds = ds
+	}
+}
+
 // WithHost supplies a libp2p Host. If not specified a new Host is created.
 func WithHost(h host.Host) Option {
-	return func(o *options) error {
-		o.h = h
-		return nil
+	return func(o *options) {
+		o.host = h
 	}
 }
 
-// WithHttpListenAddr specifies the listen address of the HTTP server that
-// serves IPNI addresses. If not specified, the default "0.0.0.0:8080" is used.
-func WithHttpListenAddr(a string) Option {
-	return func(o *options) error {
-		if a != "" {
-			o.httpListenAddr = a
-		}
-		return nil
-	}
-}
-
-// WithHttpIndexerURL configures the indexer base URL for sending announce
-// messages to. This URL is also used for metadata lookup. If a different
-// URL is needed for lookup, specify using WithHttpFindURL. If no values
-// are specified, then defaultIndexerURL is used.
-func WithHttpIndexerURL(a string) Option {
-	return func(o *options) error {
-		if a != "" {
-			o.httpIndexerURL = a
-		}
-		return nil
+// WithHttpClient supplies an optional HTTP client. If not specified, the
+// default client is used.
+func WithHttpClient(c *http.Client) Option {
+	return func(o *options) {
+		o.httpClient = c
 	}
 }
 
@@ -129,76 +119,74 @@ func WithHttpIndexerURL(a string) Option {
 // WithHttpIndexerURL. This may be needed when the lookup URL has a different
 // port or is a dhstore URL.
 func WithHttpFindURL(a string) Option {
-	return func(o *options) error {
+	return func(o *options) {
 		if a != "" {
 			o.httpFindURL = a
 		}
-		return nil
 	}
 }
 
-// WithHttpClient supplies an optional HTTP client. If not specified, the
-// default client is used.
-func WithHttpClient(c *http.Client) Option {
-	return func(o *options) error {
-		o.httpClient = c
-		return nil
+// WithHttpIndexerURL configures the indexer base URL for sending announce
+// messages to. This URL is also used for metadata lookup. If a different
+// URL is needed for lookup, specify using WithHttpFindURL. If no values
+// are specified, then defaultIndexerURL is used.
+func WithHttpIndexerURL(a string) Option {
+	return func(o *options) {
+		if a != "" {
+			o.httpIndexerURL = a
+		}
+	}
+}
+
+// WithHttpListenAddr specifies the listen address of the HTTP server that
+// serves IPNI addresses. If not specified, the default "0.0.0.0:8080" is used.
+func WithHttpListenAddr(a string) Option {
+	return func(o *options) {
+		if a != "" {
+			o.httpListenAddr = a
+		}
 	}
 }
 
 func WithLinkSystem(ls *ipld.LinkSystem) Option {
-	return func(o *options) error {
-		o.ls = ls
-		return nil
-	}
-}
-
-func WithDatastore(ds datastore.Datastore) Option {
-	return func(o *options) error {
-		o.ds = ds
-		return nil
+	return func(o *options) {
+		o.linkSys = ls
 	}
 }
 
 // WithReaderPrivacy enables (true) or disables (false) reader-privacy. This is
 // enabled by default.
 func WithReaderPrivacy(enabled bool) Option {
-	return func(o *options) error {
+	return func(o *options) {
 		o.readPriv = enabled
-		return nil
 	}
 }
 
-func newPublishOptions(o ...PublishOption) (*publishOptions, error) {
+func newPublishOptions(o ...PublishOption) *publishOptions {
 	opts := publishOptions{
 		eol:         time.Now().Add(24 * time.Hour),
 		embedPubKey: true,
 	}
 	for _, apply := range o {
-		if err := apply(&opts); err != nil {
-			return nil, err
-		}
+		apply(&opts)
 	}
-	return &opts, nil
+	return &opts
+}
+
+func WithEmbedPublicKey(epk bool) PublishOption {
+	return func(o *publishOptions) {
+		o.embedPubKey = epk
+	}
 }
 
 func WithEOL(eol time.Time) PublishOption {
-	return func(o *publishOptions) error {
+	return func(o *publishOptions) {
 		o.eol = eol
-		return nil
 	}
 }
 
 func WithTTL(ttl time.Duration) PublishOption {
-	return func(o *publishOptions) error {
+	return func(o *publishOptions) {
 		o.ttl = ttl
-		return nil
-	}
-}
-
-func WithEmbedPublicKey(epk bool) PublishOption {
-	return func(o *publishOptions) error {
-		o.embedPubKey = epk
-		return nil
 	}
 }
